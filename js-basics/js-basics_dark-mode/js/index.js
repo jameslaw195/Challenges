00d007d505ecc5be0ac55fa5cad8758556292631
James Lawrence <jameslaw195@gmail.com>
2023-11-06 13:36:16 +0100

    complete dark

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,13 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkBtn = document.querySelector('[data-js="dark-mode-button"]');
+const lightBtn = document.querySelector('[data-js="light-mode-button"]');
+const toggleBtn = document.querySelector('[data-js="toggle-button"]');
+
+// console.log(darkBtn);
+
+darkBtn.addEventListener("click", function () {
+  bodyElement.classList.add("dark");
+});
