de83e9fcff2b45ca13639df7d63b2f7c34f09bd7
James Lawrence <jameslaw195@gmail.com>
2023-11-07 11:05:14 +0100

    complete exercise 1-4

--- js-arrays-and-objects/js-objects-and-arrays_arrays/index.js
+++ js-arrays-and-objects/js-objects-and-arrays_arrays/index.js
@@ -4,18 +4,27 @@ console.clear();
 // Modify the array exampleArray so that it contains a number and a string.
 
 const exampleArray = [true];
+exampleArray.pop();
+exampleArray.push(1, "hello");
+
+console.log(exampleArray);
 
 // EXERCISE 2
 // Nest an array inside array nestedArray. After that nestedArray should contain an array as an element.
 
 const nestedArray = ["example", 10, true];
+nestedArray.push([1, 2]);
+
+console.log(nestedArray);
 
 // EXERCISE 3
 // Change the value of firstNumber to equal the first value of the numbers array using bracket notation.
 
 const numbers = [20, 10, 50];
 
-const firstNumber = "change me";
+const firstNumber = numbers[0];
+
+console.log(firstNumber);
 
 // EXERCISE 4
 // Change the fruit stored at index 0 of fruits to mango.
