a64f2e89b0d6a58f67af5284a18b4a4684a07ea4
James Lawrence <jameslaw195@gmail.com>
2023-11-07 18:39:51 +0100

    x

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -9,18 +9,34 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = pizzaInput1.value;
+  let pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = pizzaInput1.value;
+  let pizzaSize2 = pizzaInput2.value;
+
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 // Task 1
 // define the function calculatePizzaGain here
 
+function calculatePizzaGain(diameter1, diameter2) {
+  const areaOfPizza1 = (diameter1 / 2) ** 2 * Math.PI;
+  const areaofPizza2 = (diameter2 / 2) ** 2 * Math.PI;
+
+  const pizzaGain = ((areaofPizza2 - areaOfPizza1) / areaOfPizza1) * 100;
+
+  output.textContent = Math.round(pizzaGain);
+}
+
 // Task 2
 // define the function updatePizzaDisplay here
 
+function updatePizzaDisplay(pizzaElement, newSize) {}
 // Task 3
 // define the function updateOutputColor here
