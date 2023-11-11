e7afc7f8f9f8f35fae099a46fe903bb506e09747
James Lawrence <jameslaw195@gmail.com>
2023-11-11 13:32:18 +0100

    challenge complete

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.target.elements;
+  console.log(formElements.firstName.value);
+  console.log(formElements.lastName.value);
+  console.log(formElements.age.value);
+  console.log(formElements.email.value);
+  console.log(formElements.complaint.value);
+  console.log(formElements.details.value);
+  console.log(formElements.badness.value);
+  console.log(formElements.orderDate.value);
+  console.log(formElements.tos.checked);
+
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
