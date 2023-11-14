289b6f8753e82840fe008d130bd745a88953a319
James Lawrence <jameslaw195@gmail.com>
2023-11-14 17:26:12 +0100

    challenge complete

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -2,11 +2,15 @@ console.clear();
 
 // `while` loop
 
-let number = 0;
+let number = Math.random();
 let count = 0;
 
 // --v-- write/change code here --v--
-
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random();
+}
 // --^-- write/change code here --^--
 
 console.log(
