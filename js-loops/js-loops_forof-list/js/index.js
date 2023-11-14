e72f4e81e3c81ecfb216992da709121feedf341d
James Lawrence <jameslaw195@gmail.com>
2023-11-14 15:46:36 +0100

    challenge complete

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -3,9 +3,9 @@ console.clear();
 const main = document.querySelector('[data-js="main"]');
 
 // Create a list from an array
-
 const ol = document.createElement("ol");
 main.append(ol);
+// console.log(main.append(ol))
 
 const programmingLanguages = [
   "JavaScript",
@@ -18,5 +18,10 @@ const programmingLanguages = [
 ];
 
 // --v-- write/change code here --v--
+for (const language of programmingLanguages) {
+  const item = document.createElement("li");
+  item.innerHTML = language;
+  ol.append(item);
+}
 
 // --^-- write/change code here --^--
