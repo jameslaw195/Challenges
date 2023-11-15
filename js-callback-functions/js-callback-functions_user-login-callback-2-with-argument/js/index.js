280f4909913c375061804bc67a3f5174e7a8a6c4
James Lawrence <jameslaw195@gmail.com>
2023-11-15 10:42:45 +0100

    write named callback function

--- js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -4,4 +4,10 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe");
 }
 
+function showWelcomeMessage(username) {
+  console.log("Welcome " + username + " you are logged in now.");
+}
+
+handleUserLogin(showWelcomeMessage);
+
 // The exercise starts here!
