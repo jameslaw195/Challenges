35cdb1f3fd8cd64a32df7bbdbec17d11ceebd07f
James Lawrence <jameslaw195@gmail.com>
2023-11-15 11:07:03 +0100

    challenge complete

--- js-callback-functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -4,4 +4,13 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe", "admin");
 }
 
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName} you are now logged in as ${userRole}.`);
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName} you are now logged in as ${userRole}.`);
+});
 // The exercise starts here!
