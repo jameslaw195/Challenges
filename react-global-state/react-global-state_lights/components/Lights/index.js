a8565cf7d33667055d27bf7829080824f2c5f22d
James Lawrence <jameslaw195@gmail.com>
2023-12-11 20:12:35 +0100

    refactor lights

--- react-global-state/react-global-state_lights/components/Lights/index.js
+++ react-global-state/react-global-state_lights/components/Lights/index.js
@@ -11,33 +11,56 @@ const StyledLights = styled.ul`
   justify-content: center;
 `;
 
-export default function Lights() {
+export default function Lights(handleToggle) {
+  const lights = [
+    { name: "Living Room", isOn: false },
+    { name: "Kitchen", isOn: false },
+    { name: "Bedroom", isOn: false },
+    { name: "Bathroom", isOn: false },
+    { name: "Garage", isOn: false },
+    { name: "Porch", isOn: false },
+    { name: "Garden", isOn: false },
+    { name: "Office", isOn: false },
+  ];
+
   return (
     <StyledLights>
-      <li>
-        <Light name="Living Room" />
-      </li>
-      <li>
-        <Light name="Kitchen" />
-      </li>
-      <li>
-        <Light name="Bedroom" />
-      </li>
-      <li>
-        <Light name="Bathroom" />
-      </li>
-      <li>
-        <Light name="Garage" />
-      </li>
-      <li>
-        <Light name="Porch" />
-      </li>
-      <li>
-        <Light name="Garden" />
-      </li>
-      <li>
-        <Light name="Office" />
-      </li>
+      {lights.map((light) => (
+        <li>
+          <Light name={light.name} onToggle={handleToggle(light.id)} />
+        </li>
+      ))}
     </StyledLights>
   );
+}
+
+{
+  /* //   return (
+//     <StyledLights>
+//       <li>
+//         <Light name="Living Room" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Kitchen" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Bedroom" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Bathroom" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Garage" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Porch" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Garden" onToggle={handleToggle(light.id)} />
+//       </li>
+//       <li>
+//         <Light name="Office" onToggle={handleToggle(light.id)} />
+//       </li>
+//     </StyledLights>
+//   ); */
 }
