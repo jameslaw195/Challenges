d09786a6329b34678d31fa4abf1296e2ac51928d
James Lawrence <jameslaw195@gmail.com>
2023-12-12 13:46:56 +0100

    work on lights

--- react-global-state/react-global-state_lights/components/Lights/index.js
+++ react-global-state/react-global-state_lights/components/Lights/index.js
@@ -13,21 +13,21 @@ const StyledLights = styled.ul`
 
 export default function Lights(handleToggle) {
   const lights = [
-    { name: "Living Room", isOn: false },
-    { name: "Kitchen", isOn: false },
-    { name: "Bedroom", isOn: false },
-    { name: "Bathroom", isOn: false },
-    { name: "Garage", isOn: false },
-    { name: "Porch", isOn: false },
-    { name: "Garden", isOn: false },
-    { name: "Office", isOn: false },
+    { id: 1, name: "Living Room", isOn: false },
+    { id: 2, name: "Kitchen", isOn: false },
+    { id: 3, name: "Bedroom", isOn: false },
+    { id: 4, name: "Bathroom", isOn: false },
+    { id: 5, name: "Garage", isOn: false },
+    { id: 6, name: "Porch", isOn: false },
+    { id: 7, name: "Garden", isOn: false },
+    { id: 8, name: "Office", isOn: false },
   ];
 
   return (
     <StyledLights>
       {lights.map((light) => (
-        <li>
-          <Light name={light.name} onToggle={handleToggle(light.id)} />
+        <li key={id}>
+          <Light name={light.name} onToggle={handleToggle} />
         </li>
       ))}
     </StyledLights>
